e2c8ef3c7dd87e7ff3debd6d869b8f07cc14efcc
abhishek7002641genz-star <abhishek7002641genz@gmail.com>
2026-03-10 09:28:40 +0530

    Added Level1 Problem1 solution

---
+++ level1/Problem1.java
@@ -0,0 +1,16 @@
+// Program to find Harry's age in 2024
+
+public class problem1 {
+    public static void main(String[] args) {
+
+        // Declare variables
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        // Calculate age
+        int age = currentYear - birthYear;
+
+        // Display result
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
