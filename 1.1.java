1ba24100a3efa7cd0ec408e331c0fe54b72b31d9
abhishek7002641genz-star <abhishek7002641genz@gmail.com>
2026-03-10 12:48:25 +0530

    Delete 1.1.java

--- 1.1.java
+++
@@ -1,8 +0,0 @@
-public class Main 1.1{
-public static void main(String[] args) {
-int birthYear = 2000;
-int currentYear = 2024;
-int age = currentYear - birthYear;
-System.out.println(&quot;Harry&#39;s age in 2024 is &quot; + age);
-}
-}
