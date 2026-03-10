b5fe904633fb0b11cc6830890314ca6735e9b5a7
abhishek7002641genz-star <abhishek7002641genz@gmail.com>
2026-03-10 09:37:12 +0530

    Added Level1 Problem2 solution

---
+++ level1/Problem2.java
@@ -0,0 +1,17 @@
+// Program to calculate average marks of Sam in PCM
+
+public class Problem2 {
+    public static void main(String[] args) {
+
+        // Declare variables for marks
+        int mathsMarks = 94;
+        int physicsMarks = 95;
+        int chemistryMarks = 96;
+
+        // Calculate average
+        double averageMarks = (mathsMarks + physicsMarks + chemistryMarks) / 3.0;
+
+        // Display result
+        System.out.println("Sam’s average mark in PCM is " + averageMarks);
+    }
+}
